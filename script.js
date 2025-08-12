108b1c5fb59fafc0c85c134488034e3817f52047
NishithPandey <pnishith215@gmail.com>
2025-08-12 14:41:50 +0530

    added code related to http

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const fs = require('node:fs');
+// const fs = require('node:fs');
 
-fs.appendFile("hi.txt" , " I'm good ", function(err){
-    if( err )console.log(err);
-    else console.log("done");
+// fs.appendFile("hi.txt" , " I'm good ", function(err){
+//     if( err )console.log(err);
+//     else console.log("done");
+// })
+
+const http = require('http');
+const server = http.createServer(function(req, res){
+    res.end("hellow world");
+
 })
+
+server.listen(3000);
