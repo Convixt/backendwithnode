b818d551da595988988c700112de0b2ef0814cfe
NishithPandey <pnishith215@gmail.com>
2025-08-13 13:53:39 +0530

    added css and js files

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.get("/", (req,res) => {
-    res.send("Chal raha hai ");
+    res.render("index");
 });
 
 app.listen(3000, ()=>{
